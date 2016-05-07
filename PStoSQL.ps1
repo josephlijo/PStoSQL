eacaad9ec937bd6b9c88d8336d52ebc722811be6
LJ <ljnotes@outlook.com>
2016-05-07 21:43:33 +0300

    Include functionality to save the data in Excel file

--- PStoSQL.ps1
+++ PStoSQL.ps1
@@ -9,6 +9,8 @@
 
 # Variables - begins
 
+## Database variables
+
 # Set the Server name here 
 # Named instances can be set like servername\instancename 
 $SQLServer = "MyServer\MyInstance"
@@ -22,7 +24,18 @@ $SQLConnectionString = $null
 # Set the SQL query here
 $SQLQuery = "SELECT * FROM dbo.syscomments(NOLOCK)"
 
-# SQL Connection, Command, Adapter and dataset object 
+## File and Directory information 
+
+# Should save as file
+$ResultToExcel = $true;
+
+# Directory where the file should be saved to
+$ResultDirectory = 'C:\Temp\'
+
+# File name 
+$ResultFileName = "MyDataSet"
+
+## SQL Connection, Command, Adapter and dataset object 
 $SQLConnectionObj = $null
 $SQLCommandObj = $null
 $SQLDataAdapterObj = $null
@@ -68,4 +81,46 @@ try
 catch 
 {
     'An error occured while connecting to the Server - '  + $SQLServer + ' - with connection string - ' + $SQLConnectionString + '; Exception: - ' + $_.Exception.Message
+}
+
+# Export the data to Excel 
+if($ResultToExcel) 
+{
+    try
+    {
+        ## Variables 
+        $ExcelObj = $null
+        $ExcelWorkBook = $null
+        $ExcelWorkSheet = $null
+        $ExcelWorkSheetCurrent = 1
+        $ExcelFilePath = $null
+
+        # Create Excel Application object 
+        $ExcelObj = New-Object -ComObject Excel.Application
+        # 1 / True = Visible; 0 / False = No
+        $ExcelObj.Visible = $false
+
+        # Create the Work book and sheet 
+        $ExcelWorkBook = $ExcelObj.Workbooks.Add()
+        # By default, we will have 3 worksheets created
+        $ExcelWorkSheet = $ExcelWorkBook.Worksheets.Item($ExcelWorkSheetCurrent)
+
+        # Save the file
+        $ExcelFilePath = "$ResultDirectory$ResultFileName.xlsx"
+
+        # Delete the file if already exists 
+        if(Test-Path $ExcelFilePath)
+        {
+            Remove-Item $ExcelFilePath
+        }
+        $ExcelWorkBook.SaveAs($ExcelFilePath)
+
+        # Clean-up
+        $ExcelWorkBook.Close()
+        $ExcelObj.Quit()
+    }
+    catch
+    {
+        'An error occured while saving the file - '  + $_.Exception.Message
+    }
 }
