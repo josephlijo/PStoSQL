222ece328d1f153222be5945de1ab06f558548f9
LJ <ljnotes@outlook.com>
2016-05-07 14:27:38 +0300

    Initial Version with description of the script and folder structure

---
+++ PStoSQL.ps1
@@ -0,0 +1,14 @@
+# Name: PS to SQL (PowerShell to SQL) 
+# Author: LJ
+# Date: 07-May-2016
+# Description: PS script to make connection to SQL Server database, read data and write data back based on the passed in configuration. 
+
+# Uncomment the following if the script should be run in admin mode
+# #Requires -RunAsAdministrator
+
+# Variables - begins
+
+$Server = ""
+$DatabaseName = ""
+
+# Variables - ends
